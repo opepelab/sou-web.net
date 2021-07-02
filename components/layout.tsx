ab7f93c896a223767ebf8c6fd82df5f28ad05282
sou <opepelab@gmail.com>
2021-07-03 01:58:12 +0900

    first commit

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,7 +4,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'S.watanabe'
+const name = 'S.watana'
 export const siteTitle = 'S.watanabe WEB'
 
 export default function Layout({ children, home }) {
