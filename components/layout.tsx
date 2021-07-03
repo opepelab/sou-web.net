479d620b0dc5f63f0245a0ea108a05f57b4455bd
sou <opepelab@gmail.com>
2021-07-03 18:38:01 +0900

    first commit

--- components/layout.tsx
+++ components/layout.tsx
@@ -16,6 +16,7 @@ export default function Layout({ children, home }) {
           name="description"
           content="Learn how to build a personal website using Next.js"
         />
+        <meta name="viewport" content="width=device-width,initial-scale=1"/>
         <meta
           property="og:image"
           content={`https://og-image.vercel.app/${encodeURI(
