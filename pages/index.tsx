80d5a742906c6f9bb90ada1d6bacf909a8aacae4
sou <opepelab@gmail.com>
2021-07-04 15:51:00 +0900

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,11 +16,11 @@ export default function Index() {
       <Link href="https://github.com/opepelab">
         <Image src="/images/gitx.png" width={100} height={100} />
       </Link>
-      <table width="4%"></table>
+
       <Link href="https://opepelab.org">
         <Image src="/images/wpss.png" width={100} height={100} />
       </Link>
-      <table width="4%"></table>
+
       <Link href="https://www.linkedin.com/in/s-watanabe-a25157205">
         <Image src="/images/linkedx.png" width={100} height={100} />
       </Link>
