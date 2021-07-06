7959ffcdc0d78bca66afded3f56706f0ca277f99
sou <opepelab@gmail.com>
2021-07-07 06:18:37 +0900

    first commit

--- pages/abc.tsx
+++ pages/abc.tsx
@@ -1,9 +1,26 @@
-import { useAmp } from "next/amp";
-export const config = {
-  amp: "hybrid",
-};
+import { useAmp } from 'next/amp'
 
-export default () => {
-  const isAmp = useAmp();
-  return <div>index: {isAmp ? "amp" : "normal"}</div>;
-};
+export const config = { amp: 'hybrid' }
+
+function About(props) {
+  const isAmp = useAmp()
+
+  return (
+    <div>
+      <h3>My AMP About Page!</h3>
+      {isAmp ? (
+        <amp-img
+          width="300"
+          height="300"
+          src="/my-img.jpg"
+          alt="a cool image"
+          layout="responsive"
+        />
+      ) : (
+        <img width="300" height="300" src="/my-img.jpg" alt="a cool image" />
+      )}
+    </div>
+  )
+}
+
+export default About
