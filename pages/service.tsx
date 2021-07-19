0bd7bde78b9aff9fa0f299cbb9d46f3ccd4b6b04
sou <opepelab@gmail.com>
2021-07-19 14:43:15 +0900

    first commit

--- pages/service.tsx
+++ pages/service.tsx
@@ -12,7 +12,7 @@ export default function Index() {
       </Head>
       <main>
       <a href="https://github.com/opepelab/C-x9/blob/main/C-x9.c" target="_blank">入力に9掛けて9個出力(C言語)</a><br />
-        <a href="https://ssgnu.vercel.app/" target="_blank">Nuxt.js Static Site</a>
+        <a href="https://ssgnux.vercel.app/" target="_blank">Nuxt.js Static Site</a>
       </main>
     </div>
   );
