b684d5a8a25c55f920e4ef58ff7460b6a75d2934
sou <opepelab@gmail.com>
2021-09-03 02:00:50 +0900

    first commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,10 +2,12 @@ const Header: React.FC = () => {
     return (
         <div>
             <img className="left" src="/images/ore.png" width={148} height={100} />
-        <div className="flex-container">         
-            <header>
-                <h1 id="title"><a href="/">Sou Watanabe</a></h1>
-            </header>
+        <div className="flex-container">
+            <label htmlFor="checkBtn">     
+                <header>
+                    <h1 id="title"><a href="/">Sou Watanabe</a></h1>
+                </header>
+            </label>
         </div>
         </div>
     );
--- components/PhoneMenu.tsx
+++ components/PhoneMenu.tsx
@@ -12,10 +12,9 @@ const PhoneMenu: React.FC = () => {
                 </label>
             <nav className="bg">
                 <div id="ms">
-                
-                <label htmlFor="checkBtn"><Link href="/" ><li className="tate">Home</li></Link></label>
+                <Link href="/" ><a><li className="tate">Home</li></a></Link>
                 <Link href="about"><a><li className="tate">About</li></a></Link>
-                <Link href="skills"><a><li className="tate">skiLL</li></a></Link>
+                <Link href="skills"><a><li className="tate">Skill</li></a></Link>
                 <Link href="access"><a><li className="tate">SNS</li></a></Link>
                 <Link href="service"><a><li className="tate">Prod</li></a></Link>
                 <Link href="design"><a><li className="tate">Artis</li></a></Link>
