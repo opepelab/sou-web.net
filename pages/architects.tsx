d3834314edccd9f511ebce84c4ad17483f69ce7a
sou <opepelab@gmail.com>
2021-09-21 03:41:46 +0900

    first commit

--- pages/architects.tsx
+++ pages/architects.tsx
@@ -11,7 +11,7 @@ const Architects: React.FC = () => {
       <h2>HIDE PAGE</h2>
       <div className="triangle-bottom" />
       <main>
-      <h2>「マシン命名　アイドル消費電力多いRyzen」</h2>
+      <h2>「マシン命名アイドル消費電力多いRyzen」</h2>
       <p>MotherBoard : Intel<br />
       Solid State Drive : M.2 2280 NVMe Gen5x4 <br />
       Random Access Memory : DDR5 12,600MHz 128GBx4<br />
