300d89c09181ca4329de57e7207f8afb01acb76c
sou <opepelab@gmail.com>
2021-10-07 20:45:02 +0900

    first commit

--- pages/picture.tsx
+++ pages/picture.tsx
@@ -10,7 +10,8 @@ const Picture: React.FC = () => {
       <h2>PICTURE</h2>
       <div className="triangle-bottom" />
       <main>
-      <img className="Zoom pm size" src="/picture/haruka.png" width={214} height={302} />
+      <img className="Zoom pm" src="/picture/haruka.png" width={214} height={302} />
+      <img className="Zoom pm" src="/picture/watas.png" width={214} height={302} />
       <img className="Zoom pm" src="/picture/otoko.png" width={255} height={404} />
       </main>
     </div>
