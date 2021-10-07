d8f093625e2834b6435150ae286089e439a5514c
sou <opepelab@gmail.com>
2021-10-07 18:33:01 +0900

    first commit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,22 +3,24 @@ const Footer: React.FC = () => {
     return (
         <div className="footer">
             <footer>
-                <div className="flex-container">                      
+                <div className="flex-container">        
+                
+                        <a href="https://docs.python.org/ja/3/reference/index.html">
+                            <img className="Zoom" src="/SVG/TypeScript.svg" width={50} height={50} />
+                        </a>   
+
                         <a href="https://ja.reactjs.org/docs/getting-started.html">
-                            <img className="Maru Zoom" src="/SVG/react.svg" width={50} height={50} />
+                            <img className="Zoom" src="/SVG/Next.svg" width={50} height={50} />
                         </a>
                         
                         <a href="https://developer.mozilla.org/ja/docs/Learn/Getting_started_with_the_web/HTML_basics">
-                        <img className="Maru Zoom" src="/SVG/html5.svg" width={50} height={50} />
+                        <img className="Zoom" src="/SVG/html5.svg" width={50} height={50} />
                         </a>
 
                         <a href="https://developer.mozilla.org/ja/docs/Learn/Getting_started_with_the_web/CSS_basics">
-                            <img className="Maru Zoom" src="/SVG/css3.svg" width={50} height={50} />
+                            <img className="Zoom" src="/SVG/css3.svg" width={50} height={50} />
                         </a>
 
-                        <a href="https://docs.python.org/ja/3/reference/index.html">
-                        <img className="Maru Zoom" src="/SVG/python3.svg" width={50} height={50} />
-                        </a>
                 </div>
                 <a className="black" href="/"><h5 className="inblo">Copyright © 2021 Watanabe S. inc.</h5></a> 
             </footer>
