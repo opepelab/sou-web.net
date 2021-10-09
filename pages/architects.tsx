7b1c0cd5b787f91984d5f80157eec1cf6d82032c
sou <opepelab@gmail.com>
2021-10-10 07:00:14 +0900

    first commit

---
+++ pages/architects.tsx
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+
+
+const Architects: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>Skills Watanabe S.</title>
+        <meta name="description" content="私の習得ルート"/>
+      </Head>
+      <h2>HIDE PAGE</h2>
+      <div className="triangle-bottom" />
+      <main>
+      <h2>「マシン命名アイドル消費電力多いRyzen」</h2>
+      <p>MotherBoard : Intel<br />
+      Solid State Drive : M.2 2280 NVMe Gen5x4 <br />
+      Random Access Memory : DDR5 12,600MHz 128GBx4<br />
+      Central Processing Unit : Intel</p>
+      </main>
+    </div>
+  );
+}
+
+
+
+
+export default Architects;
