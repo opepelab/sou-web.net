9d0915f87fc2fc343f2cfde86e277467108018de
sou <opepelab@gmail.com>
2021-10-10 22:08:21 +0900

    first commit

--- components/PictureHigh.tsx
+++ components/PictureHigh.tsx
@@ -5,7 +5,7 @@ const PictureHigh: React.FC = () => {
             <a className="maru" href="https://github.com/opepelab" target="_blank">
                 <img className="Maru Zoom" src="/images/gitbaju.svg" width={70} height={70} />
             </a>
-                 <img className="Maru Zoom" src="/images/orei.png" width={147} height={150} />
+                 <img className="Maru Zoom" src="/images/orei.png" width={117} height={120} />
                  
             <a className="maru" href="mailto:ocelot2828@gmail.com" target="_blank">
                  <img className="Maru Zoom" src="/images/gm.ico" width={70} height={70} />
