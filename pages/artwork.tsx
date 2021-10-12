79399407cb6ae16536f7c59b9d4846532bbbbc72
sou <opepelab@gmail.com>
2021-10-12 18:55:24 +0900

    first commit

--- pages/artwork.tsx
+++ pages/artwork.tsx
@@ -10,9 +10,9 @@ const ArtWork: React.FC = () => {
       <h2>ARTWORK</h2>
       <div className="triangle-bottom" />
       <main>
-      <img className="Zoom pm" src="/picture/haruka.png" width={255} height={358} />
-      <img className="Zoom pm" src="/picture/jett.png" width={255} height={269} />
-      <img className="Zoom pm" src="/picture/otoko.png" width={255} height={404} />
+      <img className="PZoom pm" src="/picture/haruka.png" width={255} height={358} />
+      <img className="PZoom pm" src="/picture/jett.png" width={255} height={269} />
+      <img className="PZoom pm" src="/picture/otoko.png" width={255} height={404} />
       </main>
     </div>
   );
