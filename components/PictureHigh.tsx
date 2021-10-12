05d5474d2371cb2e2f04a32ba3c8d8f28a68d5be
sou <opepelab@gmail.com>
2021-10-12 09:31:21 +0900

    first commit

--- components/PictureHigh.tsx
+++ components/PictureHigh.tsx
@@ -3,14 +3,12 @@ import Link from 'next/link'
 const PictureHigh: React.FC = () => {
     return (
         <div className="flex-container top">
-            <Link href="/contact">
-                 <a><img className="Zoom" src="/images/gm.ico" width={70} height={70} /></a>
-            </Link>
-            
-                 <img className="Maru Zoom" src="/images/orei.png" width={117} height={120} />
-
             <Link href="/files">
                 <a><img className="Zoom" src="/images/folder.svg" width={70} height={70} /></a>
+            </Link>
+                 <img className="Maru Zoom" src="/images/orei.png" width={117} height={120} />
+            <Link href="/contact">
+                 <a><img className="Zoom" src="/images/gm.ico" width={70} height={70} /></a>
             </Link>
         </div>
     );
