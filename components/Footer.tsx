4e53b2ee1e7c1ed077133e0d2d5726a469ef6ae2
sou <opepelab@gmail.com>
2021-10-12 09:22:26 +0900

    first commit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,25 +5,29 @@ const Footer: React.FC = () => {
             <footer>
                 <div className="flex-container">        
 
-                        <a href="https://typescript-jp.gitbook.io/deep-dive/">
-                            <img className="Zoom" src="/SVG/TypeScript.svg" width={50} height={50} />
-                        </a>
+                    <a href="https://typescript-jp.gitbook.io/deep-dive/">
+                        <img className="Zoom" src="/SVG/TS.svg" width={50} height={50} />
+                    </a>
 
-                        <a href="https://developer.mozilla.org/ja/docs/Web/JavaScript/">
-                            <img className="Zoom" src="/SVG/JavaScript.svg" width={50} height={50} />
-                        </a>
+                    <a href="https://developer.mozilla.org/ja/docs/Web/JavaScript/">
+                        <img className="Zoom" src="/SVG/JS.svg" width={50} height={50} />
+                    </a>
+                    
+                    <a href="https://ja.reactjs.org/">
+                        <img className="Zoom" src="/SVG/RE.svg" width={50} height={50} />
+                    </a>
 
-                        <a href="https://nextjs.org/docs/">
-                            <img className="Zoom" src="/SVG/Next.svg" width={50} height={50} />
-                        </a>
+                    <a href="https://nextjs.org/docs/">
+                        <img className="Zoom" src="/SVG/Next.svg" width={50} height={50} />
+                    </a>
                         
-                        <a href="https://developer.mozilla.org/ja/docs/Learn/Getting_started_with_the_web/HTML_basics/">
+                    <a href="https://developer.mozilla.org/ja/docs/Learn/Getting_started_with_the_web/HTML_basics/">
                         <img className="Zoom" src="/SVG/html5.svg" width={50} height={50} />
-                        </a>
+                    </a>
 
-                        <a href="https://developer.mozilla.org/ja/docs/Learn/Getting_started_with_the_web/CSS_basics/">
-                            <img className="Zoom" src="/SVG/css3.svg" width={50} height={50} />
-                        </a>
+                    <a href="https://developer.mozilla.org/ja/docs/Learn/Getting_started_with_the_web/CSS_basics/">
+                        <img className="Zoom" src="/SVG/css3.svg" width={50} height={50} />
+                    </a>
                 </div>
                 <a className="black" href="/"><h6 className="inblo">Copyright © 2021 Watanabe S. inc.</h6></a> 
             </footer>
