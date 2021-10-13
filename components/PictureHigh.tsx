9b203365b04d5bcbedd9055e0549277eb1855099
sou <opepelab@gmail.com>
2021-10-13 21:42:46 +0900

    first commit

--- components/PictureHigh.tsx
+++ components/PictureHigh.tsx
@@ -3,11 +3,11 @@ import ActiveLink from '../components/ActiveLink'
 const PictureHigh: React.FC = () => {
     return (
         <div className="flex-container MargTop">
-            <ActiveLink href="/files" activeClassName="hideState">
+            <ActiveLink href="/files" activeClassName="colorState">
                 <a><img className="Zoom" src="/images/folder.svg" width={70} height={70} /></a>
             </ActiveLink>
                  <img className="Maru Zoom" src="/images/orei.png" width={117} height={120} />
-            <ActiveLink href="/contact" activeClassName="hideState">
+            <ActiveLink href="/contact" activeClassName="colorState">
                  <a><img className="Zoom" src="/images/gm.ico" width={70} height={70} /></a>
             </ActiveLink>
         </div>
