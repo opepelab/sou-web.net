c3a3617b85e60cb5b2a42edbd849937b2117f354
sou <opepelab@gmail.com>
2021-10-13 22:32:29 +0900

    first commit

--- components/PictureHigh.tsx
+++ components/PictureHigh.tsx
@@ -6,9 +6,11 @@ const PictureHigh: React.FC = () => {
             <ActiveLink href="/files" activeClassName="colorState">
                 <a><img className="Zoom" src="/images/folder.svg" width={70} height={70} /></a>
             </ActiveLink>
-                 <img className="Maru Zoom" src="/images/orei.png" width={117} height={120} />
+            <table width="3%" />
+                <img className="Maru Zoom" src="/images/orei.png" width={117} height={120} />
+            <table width="3%" />
             <ActiveLink href="/contact" activeClassName="colorState">
-                 <a><img className="Zoom" src="/images/gm.ico" width={70} height={70} /></a>
+                <a><img className="Zoom" src="/images/gm.ico" width={70} height={70} /></a>
             </ActiveLink>
         </div>
     );
