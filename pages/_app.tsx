1a1185376884aadabff39f1fd09b388ed6360826
sou <opepelab@gmail.com>
2021-10-14 13:38:08 +0900

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,11 +5,9 @@ import '../styles/mobile.css'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }): JSX.Element => {
   return (
-    <div className="selectNone">
     <Layout>
       <Component {...pageProps} />
     </Layout>
-    </div>
   );
 }
 export default MyApp;
