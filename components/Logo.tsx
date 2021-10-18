e404e6c40135b52dbc9cf550dc3f7f6d00bf75f7
sou <opepelab@gmail.com>
2021-10-18 15:38:33 +0900

    first commit

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 const Logo: React.FC = () => {
     return (
         <div className="Logo inblo">
-            <Image className="Maru mobile" src="/images/jett.svg" width={50} height={50} />
+            <Image className="Maru mobile" src="/SVG/jett.svg" width={50} height={50} />
         </div>
     );
 }
