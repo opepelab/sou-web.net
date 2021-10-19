c183d2eb832502756c85374a33ca28c177d570a2
sou <opepelab@gmail.com>
2021-10-20 01:06:33 +0900

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,8 @@
+import {AppProps} from 'next/app'
 import Layout from '../components/layout'
 import { AnimatePresence } from "framer-motion";
 import '../styles/globals.css'
 import '../styles/mobile.css'
-
-type AppProps = {
-  Component: React.FC;
-  pageProps: string;
-  router: any;
-}
 
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps, router } ): JSX.Element => {
