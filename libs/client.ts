38083646e942f6899dbe1981cb4603859ff70af6
sou <opepelab@gmail.com>
2021-10-25 15:53:21 +0900

    first commit

--- libs/client.ts
+++ libs/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from 'microcms-js-sdk';
 
 const client = createClient({
-  serviceDomain: process.env.SERVICE_DOMAIN,
-  apiKey: process.env.API_KEY,
+  serviceDomain: process.env.NODE_ENV,
+  apiKey: process.env.PUBLIC_URL,
 });
 
 export default client;
