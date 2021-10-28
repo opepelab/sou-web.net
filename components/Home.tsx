16de7a334d7d15282b0a84b576f6feaa3871baad
sou <opepelab@gmail.com>
2021-10-28 18:58:51 +0900

    first commit

--- components/Home.tsx
+++ components/Home.tsx
@@ -7,16 +7,10 @@ const Home: React.FC<Props> = ({ open, setOpen }) => {
     return (     
         <div>
             <main>
-                <h1 className="Home">Sou Web</h1>
-            <div aria-expanded={open} onClick={() => setOpen(!open)}
+                <div aria-expanded={open} onClick={() => setOpen(!open)}
                 aria-label="TOP Toggle menu">
-            <div className="loading">
-                <div className="loading-dot"></div>
-                <div className="loading-dot"></div>
-                <div className="loading-dot"></div>
-                <div className="loading-dot"></div>
-            </div>
-            </div>
+                <h1 className="Home">Sou Web</h1>
+                </div>
             </main>
         </div>
     );
