12e5a7b4b70958e9a6665716629bf8ac36d5a0ff
sou <opepelab@gmail.com>
2021-10-31 19:10:34 +0900

    first commit

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,4 +1,3 @@
-import client from "../libs/client";
 import { GetStaticProps } from "next"
 import { motion } from "framer-motion"
 import Layout from '../components/Layout/layout'
@@ -27,10 +26,10 @@ const Blog: React.FC<Map> = ({blog}) => {
           <meta name="description" content="ログ"/>
         </Head>
         <main className="textLeft margin50p">
-          {blog.map((props: Content) => (
-          <dl key={props.id}>
-            <dt><Date dateString={props.publishedAt}/></dt>
-            <dd><Link href={`/blog/${props.id}`}><a className="scale pinkLinks">{props.title}</a></Link></dd>
+          {blog.map((blog: Content) => (
+          <dl key={blog.id}>
+            <dt><Date dateString={blog.publishedAt}/></dt>
+            <dd><Link href={`/blog/${blog.id}`}><a className="scale pinkLinks">{blog.title}</a></Link></dd>
           </dl>
           ))}
         </main>
@@ -40,11 +39,17 @@ const Blog: React.FC<Map> = ({blog}) => {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const data = await client.get<{ contents: Content }>({ endpoint: "blog" });
+  const key = {
+    headers: {'X-MICROCMS-API-KEY': process.env.API_KEY},
+  };
+  const data = await fetch('https://sou.microcms.io/api/v1/blog?limit=1000', key)
+  .then(res => res.json())
+  .catch(() => null);
 
   return {
     props: {
       blog: data.contents,
+      totalCount: data.totalCount
     },
   };
 };
