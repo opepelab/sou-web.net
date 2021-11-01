0ed7a775f23fe91df4e7de0e0c599d5e6c6007c1
sou <opepelab@gmail.com>
2021-11-01 19:49:47 +0900

    first commit

--- globals.d.ts
+++ globals.d.ts
@@ -3,5 +3,6 @@ declare namespace NodeJS {
     readonly NODE_ENV: 'development' | 'production' | 'test';
     readonly PUBLIC_URL: string;
     readonly SERVICE_DOMAIN: string;
+    readonly API_KEY: string;
   }
 }
