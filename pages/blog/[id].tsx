b1a0723ac4baee57ef22588c02cea960e40fed5e
sou <opepelab@gmail.com>
2021-11-01 23:24:52 +0900

    first commit

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -45,7 +45,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
   const data = await fetch('https://sou.microcms.io/api/v1/blog?limit=40/', key)
     .then(res => res.json())
     .catch((err) => console.warn(err));
-  const paths: [] = data.contents.map((content: ContentId) => `/blog/${content.id}`)
+  const paths: string[] = data.contents?.map((content: ContentId) => `/blog/${content.id}`)
 
   return { 
     paths, 
