b327ada0c7ba150d52e53e396a536acf20cb0bde
sou <opepelab@gmail.com>
2021-11-01 23:19:53 +0900

    first commit

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -38,16 +38,14 @@ const Id: React.FC<Content> = ({ blog }) => {
 
 
 
-export const getStaticPaths: GetStaticPaths<{ id: string }> = async () => {
+export const getStaticPaths: GetStaticPaths = async () => {
   const key = {
     headers: {'X-MICROCMS-API-KEY': process.env.API_KEY},
   }
   const data = await fetch('https://sou.microcms.io/api/v1/blog?limit=40/', key)
     .then(res => res.json())
     .catch((err) => console.warn(err));
-  const paths: [] = data.contents?.map((content: ContentId) => ({
-    params: { id: content.id },
-  }));
+  const paths: [] = data.contents?.map((content: ContentId) => `/blog/${content.id}`)
 
   return { 
     paths, 
