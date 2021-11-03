0401f8ee1ca266305203c5bc13c042cfc95382f7
sou <opepelab@gmail.com>
2021-11-03 22:33:46 +0900

    first commit

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -7,23 +7,22 @@ type Props = {
 
 const Menu: React.FC<Props> = ({ open, setOpen }) => {
     return (     
-            <div className="MenuList">
-            {/* 表示させとくから先に書く次のコードで新たに上書きして真偽値を書き換えて描写 */}
-                    <div aria-expanded={open} onClick={() => setOpen(!open)}>
-                        <div className="CN">
-                            <nav>   
-                                <ul className="nav2">                         
-                                    <ActiveLink href="/" activeClassName="colorState"><a><li className="tate">トップ</li></a></ActiveLink>
-                                    <ActiveLink href="/about" activeClassName="colorState"><a><li className="tate">詳細</li></a></ActiveLink>
-                                    <ActiveLink href="/links" activeClassName="colorState"><a><li className="tate">リンク</li></a></ActiveLink>
-                                    <ActiveLink href="/picture" activeClassName="colorState"><a><li className="tate">写真</li></a></ActiveLink>
-                                    <ActiveLink href="/designenv" activeClassName="colorState"><a><li className="tate">Design.Env</li></a></ActiveLink>
-                                    <ActiveLink href="/developenv" activeClassName="colorState"><a><li className="tate">Devlop.Env</li></a></ActiveLink>
-                                </ul> 
-                            </nav>
-                        </div>
+        <div className="MenuList">
+            <div aria-expanded={open} onClick={() => setOpen(!open)}>
+                    <div className="CN">
+                        <nav>   
+                            <ul className="nav2">                         
+                                <ActiveLink href="/" activeClassName="colorState"><a><li className="tate">トップ</li></a></ActiveLink>
+                                <ActiveLink href="/about" activeClassName="colorState"><a><li className="tate">詳細</li></a></ActiveLink>
+                                <ActiveLink href="/links" activeClassName="colorState"><a><li className="tate">リンク</li></a></ActiveLink>
+                                <ActiveLink href="/picture" activeClassName="colorState"><a><li className="tate">写真</li></a></ActiveLink>
+                                <ActiveLink href="/designenv" activeClassName="colorState"><a><li className="tate">Design.Env</li></a></ActiveLink>
+                                <ActiveLink href="/developenv" activeClassName="colorState"><a><li className="tate">Devlop.Env</li></a></ActiveLink>
+                            </ul> 
+                        </nav>
                     </div>
             </div>
+        </div>
     );
 };
 
--- globals.d.ts
+++ globals.d.ts
@@ -2,7 +2,6 @@ declare namespace NodeJS {
   interface ProcessEnv {
     readonly NODE_ENV: 'development' | 'production' | 'test';
     readonly PUBLIC_URL: string;
-    readonly SERVICE_DOMAIN: string;
     readonly API_KEY: string;
   }
 }
