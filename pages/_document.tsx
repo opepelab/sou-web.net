622883b111a80f8ddb00e29ca64bc4b02122cd7f
sou <opepelab@gmail.com>
2021-11-06 02:14:08 +0900

    first commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ class MyDocument extends Document {
     return (
       <Html lang="JA">
         <Head>
-        <link href="/azur.ico" rel="icon" />
+        <link href="/logo.png" rel="icon" />
         <link href="https://fonts.googleapis.com/css2?family=Zen+Kurenaido&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Herr+Von+Muellerhoff&display=swap" rel="stylesheet" />
         </Head>
