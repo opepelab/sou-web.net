00f9091431ad5928a79af354fe19998c4d3ba85d
sou <opepelab@gmail.com>
2021-11-15 14:22:22 +0900

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,7 +15,6 @@ const MyApp = ({ Component, pageProps, router }: AppProps): JSX.Element => {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="robots" content="noindex,nofollow" />
       </Head>
       
       <ThemeContext.Provider value={{theme, toggleTheme}}>
