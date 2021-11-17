6a500be217a3cb171e31b1c28e07fb6f0fcdf54b
sou <opepelab@gmail.com>
2021-11-17 20:23:29 +0900

    first commit

--- components/ThemeToggler.tsx
+++ components/ThemeToggler.tsx
@@ -15,7 +15,7 @@ export const ThemeToggler = () => {
     return (
         <div className="Logo inblo">
                 <div aria-expanded={trans} onClick={() => {toggleTheme(theme === 'dark' ? 'light' : 'dark');setTrans(!trans)}}>
-               <Image src="/aa.png" width={40} height={40} />
+               <Image className="m0" src="/logo.png" width={60} height={60} />
             </div>
         </div>
     );
