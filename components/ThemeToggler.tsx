ad658783a6aae6792608dca44c2e231ba12e1950
sou <opepelab@gmail.com>
2022-01-05 23:41:20 +0900

    first commit

--- components/ThemeToggler.tsx
+++ components/ThemeToggler.tsx
@@ -15,7 +15,7 @@ export const ThemeToggler = () => {
       <div
         aria-expanded={trans}
         onClick={() => {
-          toggleTheme(theme === "light" ? "dark" : "light");
+          toggleTheme(theme === "dark" ? "light" : "dark");
           setTrans(!trans);
         }}
       >
