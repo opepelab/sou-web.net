0bf4df53122145687f98105c1d51884a68775b19
sou <opepelab@gmail.com>
2022-01-26 00:04:23 +0900

    commit

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -6,7 +6,7 @@ export default async (req, res) => {
     return res.status(404).end();
   }
   const content = await fetch(
-    `https://sou.microcms.io/api/v1/blog/${req.query.slug}?fields=id&draftKey=${req.query.draftKey}`,
+    `https://sou.microcms.io/api/v1/blog/${req.query.id}?fields=id&draftKey=${req.query.draftKey}`,
     { headers: { "X-MICROCMS-API-KEY": process.env.API_KEY || "" } }
   )
     .then((res) => res.json())
