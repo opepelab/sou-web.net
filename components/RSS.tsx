13d399a7a28ab0174d3b0af127a49a2315e92029
sou <ocelot2828@gmail.com>
2022-02-07 06:53:51 +0900

    fix

--- components/RSS.tsx
+++ components/RSS.tsx
@@ -4,15 +4,13 @@ const RSS: React.FC = () => {
   return (
     <div className="RSS">
       <Link href="/feed">
-        <a>
-          <div>
-            <div className="feedicon">
-              <div className="f1"></div>
-              <div className="f2"></div>
-              <div className="f3"></div>
-            </div>
+        <div tabIndex={0}>
+          <div className="feedicon">
+            <div className="f1"></div>
+            <div className="f2"></div>
+            <div className="f3"></div>
           </div>
-        </a>
+        </div>
       </Link>
     </div>
   );
