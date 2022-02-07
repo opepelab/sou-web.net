fc080fe4a720eeb1fcc230ac65f1449eeeb66cfc
sou <ocelot2828@gmail.com>
2022-02-07 13:57:07 +0900

    f

--- components/RSS.tsx
+++ components/RSS.tsx
@@ -3,7 +3,15 @@ import Link from "next/link";
 const RSS: React.FC = () => {
   return (
     <div className="RSS">
-      <Link href="/feed">RSS</Link>
+      <Link href="/feed">
+        <div tabIndex={0}>
+          <div className="feedicon">
+            <div className="f1"></div>
+            <div className="f2"></div>
+            <div className="f3"></div>
+          </div>
+        </div>
+      </Link>
     </div>
   );
 };
