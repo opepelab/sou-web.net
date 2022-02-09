5e2b895e2fac1ff361dddb535e89a2026f8d7eaa
sou <ocelot2828@gmail.com>
2022-02-10 00:13:05 +0900

    A

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -11,17 +11,17 @@ const Menu = () => {
   return (
     <div>
       <div className="Line">
-        <div ref={menuRef} tabIndex={0} onBlur={() => setOpen(false)}>
-          <button
-            aria-expanded={open}
-            onMouseDown={() => setOpen(!open)}
-            aria-label="Toggle mobile navigation menu"
-            type="button"
-          >
-            <span />
-            <span />
-            <span />
-          </button>
+        <button
+          aria-expanded={open}
+          onMouseDown={() => setOpen(!open)}
+          aria-label="Toggle mobile navigation menu"
+          type="button"
+        >
+          <span />
+          <span />
+          <span />
+        </button>
+        <div ref={menuRef} tabIndex={0} onBlur={() => setOpen(false)} onFocus={() => setOpen(true)}>
           <div className="MenuList">
             <div aria-expanded={open}>
               <nav>
