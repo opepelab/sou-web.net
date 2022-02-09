20cf69484ce8c85d62b9b2481eebdf2d9640b672
sou <ocelot2828@gmail.com>
2022-02-10 00:19:39 +0900

    A

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -21,7 +21,7 @@ const Menu = () => {
           <span />
           <span />
         </button>
-        <div ref={menuRef} tabIndex={0} onBlur={() => setOpen(false)} onFocus={() => setOpen(true)}>
+        <div ref={menuRef} tabIndex={0} onBlur={() => setOpen(false)} onFocus={() => setOpen(open)}>
           <div className="MenuList">
             <div aria-expanded={open}>
               <nav>
