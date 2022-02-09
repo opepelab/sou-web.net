1e7da6222b11800bcb2f985bce1e3aa07514585c
sou <ocelot2828@gmail.com>
2022-02-09 23:25:35 +0900

    AA

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -23,7 +23,7 @@ const Menu = () => {
             <span />
           </button>
           <div className="MenuList">
-            <div aria-expanded={open} onClick={() => setOpen(open)}>
+            <div aria-expanded={open} onClick={() => setOpen(true)}>
               <nav>
                 <ul className="nav2">
                   <li id="About">
