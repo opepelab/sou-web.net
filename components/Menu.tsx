c52943aad0239feaedf31df084e531f5e17d3960
sou <ocelot2828@gmail.com>
2022-02-09 17:23:09 +0900

    AAA

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -23,106 +23,108 @@ const Menu = () => {
             <span />
           </button>
           <div className="MenuList">
-            <nav>
-              <ul className="nav2">
-                <li id="About">
-                  <label htmlFor="toggle1">
-                    <a className="left1">
-                      <div className="gg-details-more" />
-                      About
-                      <div className="SankakuWhite" />
-                    </a>
-                  </label>
-                  <input type="checkbox" id="toggle1" autoComplete="off" />
-                  <ul>
-                    <li>
-                      <ActiveLink href="/profile" activeClassName="headerState">
-                        <a>Summary</a>
-                      </ActiveLink>
-                    </li>
-                    <li>
-                      <ActiveLink href="/about" activeClassName="headerState">
-                        <a>Overview</a>
-                      </ActiveLink>
-                    </li>
-                  </ul>
-                </li>
-                <li id="Works">
-                  <label htmlFor="toggle2">
-                    <a className="right2">
-                      <div className="gg-style" />
-                      Link
-                      <div className="SankakuWhite" />
-                    </a>
-                  </label>
-                  <input type="checkbox" id="toggle2" autoComplete="off" />
-                  <ul>
-                    <li>
-                      <a href="https://github.com/opepelab" target="_blank">
-                        GitHub
+            <div aria-expanded={open} onClick={() => setOpen(!open)}>
+              <nav>
+                <ul className="nav2">
+                  <li id="About">
+                    <label htmlFor="toggle1">
+                      <a className="left1">
+                        <div className="gg-details-more" />
+                        About
+                        <div className="SankakuWhite" />
                       </a>
-                    </li>
-                    <li>
-                      <a href="https://www.linkedin.com/in/s-watanabe-a25157205" target="_blank">
-                        LinkdIn
+                    </label>
+                    <input type="checkbox" id="toggle1" autoComplete="off" />
+                    <ul>
+                      <li>
+                        <ActiveLink href="/profile" activeClassName="headerState">
+                          <a>Summary</a>
+                        </ActiveLink>
+                      </li>
+                      <li>
+                        <ActiveLink href="/about" activeClassName="headerState">
+                          <a>Overview</a>
+                        </ActiveLink>
+                      </li>
+                    </ul>
+                  </li>
+                  <li id="Works">
+                    <label htmlFor="toggle2">
+                      <a className="right2">
+                        <div className="gg-style" />
+                        Link
+                        <div className="SankakuWhite" />
                       </a>
-                    </li>
-                    <li>
-                      <a href="https://opepelab.org/" target="_blank">
-                        WordPress
+                    </label>
+                    <input type="checkbox" id="toggle2" autoComplete="off" />
+                    <ul>
+                      <li>
+                        <a href="https://github.com/opepelab" target="_blank">
+                          GitHub
+                        </a>
+                      </li>
+                      <li>
+                        <a href="https://www.linkedin.com/in/s-watanabe-a25157205" target="_blank">
+                          LinkdIn
+                        </a>
+                      </li>
+                      <li>
+                        <a href="https://opepelab.org/" target="_blank">
+                          WordPress
+                        </a>
+                      </li>
+                      <li>
+                        <ActiveLink href="/photo" activeClassName="headerState">
+                          <a>Whiteboard</a>
+                        </ActiveLink>
+                      </li>
+                      <li>
+                        <a href="https://www.pixiv.net/users/10729947" target="_blank">
+                          Pixiv
+                        </a>
+                      </li>
+                      <li>
+                        <a href="https://www.instagram.com/sou_watana/" target="_blank">
+                          Instagram
+                        </a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <ActiveLink href="/list/1" activeClassName="headerState">
+                      <a>
+                        <div className="gg-calibrate" />
+                        Posts
                       </a>
-                    </li>
-                    <li>
-                      <ActiveLink href="/photo" activeClassName="headerState">
-                        <a>Whiteboard</a>
-                      </ActiveLink>
-                    </li>
-                    <li>
-                      <a href="https://www.pixiv.net/users/10729947" target="_blank">
-                        Pixiv
+                    </ActiveLink>
+                  </li>
+                  <li>
+                    <ActiveLink href="/mail" activeClassName="headerState">
+                      <a>
+                        <div className="gg-mail" />
+                        Mail
                       </a>
-                    </li>
-                    <li>
-                      <a href="https://www.instagram.com/sou_watana/" target="_blank">
-                        Instagram
+                    </ActiveLink>
+                  </li>
+                  <li>
+                    <ActiveLink href="/develop" activeClassName="headerState">
+                      <a className="right3">
+                        <div className="gg-attribution" />
+                        Development Environment
                       </a>
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  <ActiveLink href="/list/1" activeClassName="headerState">
-                    <a>
-                      <div className="gg-calibrate" />
-                      Posts
-                    </a>
-                  </ActiveLink>
-                </li>
-                <li>
-                  <ActiveLink href="/mail" activeClassName="headerState">
-                    <a>
-                      <div className="gg-mail" />
-                      Mail
-                    </a>
-                  </ActiveLink>
-                </li>
-                <li>
-                  <ActiveLink href="/develop" activeClassName="headerState">
-                    <a className="right3">
-                      <div className="gg-attribution" />
-                      Development Environment
-                    </a>
-                  </ActiveLink>
-                </li>
-                <li>
-                  <ActiveLink href="/privacy" activeClassName="headerState">
-                    <a className="right4">
-                      <div className="gg-coffee" />
-                      Privacy Policy
-                    </a>
-                  </ActiveLink>
-                </li>
-              </ul>
-            </nav>
+                    </ActiveLink>
+                  </li>
+                  <li>
+                    <ActiveLink href="/privacy" activeClassName="headerState">
+                      <a className="right4">
+                        <div className="gg-coffee" />
+                        Privacy Policy
+                      </a>
+                    </ActiveLink>
+                  </li>
+                </ul>
+              </nav>
+            </div>
           </div>
         </div>
       </div>
