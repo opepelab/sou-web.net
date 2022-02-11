b3d76201a25f91f6634dfdc49456b507b6455349
sou <ocelot2828@gmail.com>
2022-02-11 12:18:53 +0900

    test

--- components/Theme/Theme.ts
+++ components/Theme/Theme.ts
@@ -3,7 +3,7 @@ export const DarkTheme = {};
 export const LightTheme = {
   weak: "#666",
   strong: "#333",
-  PLink: "#009999",
+  PLink: "#CE1D51",
   f3: "solid 5px #333",
   background: "#FFF",
   bgg: "#FFEFEF",
