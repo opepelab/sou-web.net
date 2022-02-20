58e3888615ce4738c9081f889b825f75f5af49d7
sou <ocelot2828@gmail.com>
2022-02-20 18:54:57 +0900

    Testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,5 @@
-import client from "../libs/contentful";
-import { GetStaticProps } from "next";
 import Framerdiv from "../components/Sys/Framer";
-import { Entry, EntryCollection } from "contentful";
-import { IPostFields } from "../libs/types";
-import Date from "../components/Sys/date";
 import Head from "next/head";
-import Link from "next/link";
 
 type Map = {
   blog: {
@@ -31,39 +25,9 @@ const Index: React.FC<Map> = ({ blog }) => {
           のウェブサイトです。
         </p>
         <h5>最新記事</h5>
-        {blog.map((blog: Entry<IPostFields>) => (
-          <dl key={blog.sys.id}>
-            <Link href={`/docs/${blog.fields.slug}`}>
-              <a>
-                <dt className="dateST">
-                  <Date dateString={blog.fields.date} />
-                </dt>
-                <div className="PPx scaleLinks pinkLinks">{blog.fields.title}</div>
-              </a>
-            </Link>
-          </dl>
-        ))}
-        <Link href="/list/1">
-          <a className="Link-Color">
-            <h5>全部見る →</h5>
-          </a>
-        </Link>
       </main>
     </Framerdiv>
   );
-};
-
-export const getStaticProps: GetStaticProps = async () => {
-  const data: EntryCollection<IPostFields> = await client.getEntries({
-    content_type: "blog",
-    order: "-fields.date",
-    limit: 10,
-  });
-  return {
-    props: {
-      blog: data.items,
-    },
-  };
 };
 
 export default Index;
