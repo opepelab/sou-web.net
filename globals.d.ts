82bed6018acb24a7d2feef64662b1155f9f0c7b0
sou <ocelot2828@gmail.com>
2022-02-21 07:23:08 +0900

    FIx

--- globals.d.ts
+++ globals.d.ts
@@ -3,7 +3,5 @@ declare namespace NodeJS {
     readonly NODE_ENV: "development" | "production" | "test";
     readonly PUBLIC_URL: string;
     readonly API_KEY: string;
-    CONTENTFUL_SPACE_ID: string;
-    CONTENTFUL_DELIVERY_TOKEN: string;
   }
 }
