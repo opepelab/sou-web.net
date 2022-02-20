270c5487c6630eca454a8486b64ca9665f706327
sou <ocelot2828@gmail.com>
2022-02-21 03:28:16 +0900

    Fix

---
+++ pages/webclip.tsx
@@ -0,0 +1,34 @@
+import Framerdiv from "../components/Sys/Framer";
+import Head from "next/head";
+
+const Webclip: React.FC = () => {
+  return (
+    <Framerdiv>
+      <Head>
+        <title>Development Environment - sou</title>
+        <meta name="description" content="開発環境" />
+      </Head>
+      <main className="margin50p resizeimage inblo">
+        <h1>Development Environment</h1>
+        <div className="triangle-bottom" />
+        <dl>
+          <img src="/picture/IMG-0554.jpg" />
+          <dt>OS</dt>
+          <dd>Windows</dd>
+          <dt>Version</dt>
+          <dd>Git</dd>
+          <dt>IDE</dt>
+          <dd>Visual Studio Code</dd>
+          <dt>language</dt>
+          <dd>TypeScript/Sass(Scss)</dd>
+          <dt>Browser</dt>
+          <dd>Edge/Chrome/Safari</dd>
+          <dt>Host</dt>
+          <dd>Vercel/Cloudflare</dd>
+        </dl>
+      </main>
+    </Framerdiv>
+  );
+};
+
+export default Webclip;
