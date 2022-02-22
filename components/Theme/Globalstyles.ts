5d1a72ab9d8466ca0e26e241838b0d89c50938cd
sou <ocelot2828@gmail.com>
2022-02-22 17:58:05 +0900

    Fix

--- components/Theme/Globalstyles.ts
+++ components/Theme/Globalstyles.ts
@@ -26,12 +26,6 @@ export const GlobalStyles = createGlobalStyle<Props>`
     .Link-Color {
         color: ${(props) => props.theme.weak};
     }
-    a {
-        color: ${(props) => props.theme.strong};
-    }
-    .SankakuBlack {
-          border-color: ${(props) => props.theme.Sankaku};
-    }
     .blue {
         color: ${(props) => props.theme.pink};
     }
@@ -39,23 +33,26 @@ export const GlobalStyles = createGlobalStyle<Props>`
       .Line span {
         background-color: ${(props) => props.theme.strong};
     }
-    // @media screen and (min-width: 739.99px) {
-    // .toggle {
-    //     background: ${(props) => props.theme.background};
-    //     box-shadow: ${(props) => props.theme.shadow};
-    // }}
-    // .toggleMenu {
-    //     background: ${(props) => props.theme.background};
+    @media screen and (min-width: 739.99px) {
+    .toggle {
+        background: ${(props) => props.theme.background};
+        box-shadow: ${(props) => props.theme.shadow};
+    }}
+    // .MenuList [aria-expanded="true"] {
+    //     background: ${(props) => props.theme.black};
     // }
-    .MenuList [aria-expanded="true"] {
-        background: ${(props) => props.theme.black};
-    }
-    .MenuList [aria-expanded="false"] {
-                  background: ${(props) => props.theme.black};
-    }
-    .nav2 a {
-        color: ${(props) => props.theme.strong};
-    }
+    // .MenuList [aria-expanded="false"] {
+    //               background: ${(props) => props.theme.black};
+    // }
+    // .nav2 a {
+    //     color: ${(props) => props.theme.strong};
+    // }
+    //     a {
+    //     color: ${(props) => props.theme.strong};
+    // }
+    //     .SankakuBlack {
+    //       border-color: ${(props) => props.theme.Sankaku};
+    // }
 }
 `;
 
