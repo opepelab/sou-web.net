59e4270382291dc627fd0ae32b4620efcc22ac04
sou <ocelot2828@gmail.com>
2022-02-23 12:29:52 +0900

    ghost fix

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import ActiveLink from "./Sys/ActiveLink";
+import ReactDOM from "react-dom";
+import ReactSwipe from "react-swipe";
 
 const Menu = () => {
   const [open, setOpen] = useState(false);
-
+  let reactSwipeEl;
   return (
     <div>
       <div className="Line">
