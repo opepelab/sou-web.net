0a0e6960983c24b25bdc9b848a9076df3b6481f4
sou <ocelot2828@gmail.com>
2022-03-04 20:49:23 +0900

    Reduced

--- components/Theme/ThemeType.ts
+++ components/Theme/ThemeType.ts
@@ -1,17 +1,9 @@
 export type ThemeType = {
   background: string;
-  bgg: string;
-  MainBG: string;
   weak: string;
   strong: string;
   PLink: string;
-  f3: string;
-  border: string;
   h12: string;
-  Sankaku: string;
-  links: string;
-  weight: string;
   shadow: string;
-  black: string;
   pink: string;
 };
