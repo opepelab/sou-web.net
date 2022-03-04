60e362fcf4417ade2e9979e0372bb0c248330e2a
sou <ocelot2828@gmail.com>
2022-03-04 20:52:30 +0900

    Reduced

--- components/Theme/Theme.ts
+++ components/Theme/Theme.ts
@@ -1,19 +1,11 @@
 export const DarkTheme = {};
 
 export const LightTheme = {
+  background: "#FFF",
   weak: "#666",
   strong: "#333",
   PLink: "#009999",
-  f3: "solid 5px #333",
-  background: "#FFF",
-  bgg: "#FFEFEF",
-  MainBG: "#242f3d",
-  menu: "rgba(255, 255, 255, 1)",
-  border: "solid 1px #333",
   h12: "solid 1px #eaeaea",
-  Sankaku: "#333 transparent transparent transparent",
-  weight: "bold",
   shadow: "0px 0px 4px 1px gray;",
-  black: "white",
   pink: "#CE1D51",
 };
