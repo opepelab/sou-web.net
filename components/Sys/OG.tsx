b65f0f4def2be09278e7ea511c0d0900dcd40a94
sou <ocelot2828@gmail.com>
2022-03-06 02:27:45 +0900

    Test

--- components/Sys/OG.tsx
+++ components/Sys/OG.tsx
@@ -12,7 +12,7 @@ const OG = ({ title, description }: StProp) => {
       <meta property="og:title" content={title} />
       <meta property="description" content={description} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content={"`https://og-image.vercel.app/${title}.png`"} />
+      <meta property="og:image" content={"https://og-image.vercel.app/" + `${title}.png`.toString()} />
     </Head>
   );
 };
