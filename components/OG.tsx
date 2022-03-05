4b1acee77a71dbb1514b2f537f7037e111e78593
sou <ocelot2828@gmail.com>
2022-03-05 20:33:39 +0900

    Test

--- components/OG.tsx
+++ components/OG.tsx
@@ -12,8 +12,8 @@ const OG = ({ title, description }: StProp) => {
       <meta property="description" content={description} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content={`https://ogi-api.vercel.app/${title}.png`} />
-      <meta name="twitter:card" content={`https://ogi-api.vercel.app/${title}.png`} />
+      <meta property="og:image" content={`https://og-image.vercel.app/${title}.png`} />
+      <meta name="twitter:card" content="summary_large_image" />
     </Head>
   );
 };
