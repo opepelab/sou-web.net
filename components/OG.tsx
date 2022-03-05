d01e224b078292cdcb86230fa83c1b272088b54c
sou <ocelot2828@gmail.com>
2022-03-05 21:43:09 +0900

    Test

--- components/OG.tsx
+++ components/OG.tsx
@@ -12,7 +12,7 @@ const OG = ({ title, description }: StProp) => {
       <meta property="description" content={description} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content={`${process.env.baseUrl}/public/sou.png`} />
+      <meta property="og:image" content={`${process.env.SITE_URL}/public/sou.png`} />
       <meta name="twitter:card" content="summary_large_image" />
       {/* <meta name="twitter:image" content={`https://sou-web.net/sou.png`} /> */}
     </Head>
