4ac36ba041c7acb508c541f67379f9882c7eda5a
sou <ocelot2828@gmail.com>
2022-03-13 07:10:42 +0900

    ok

--- pages/konoblog.tsx
+++ pages/konoblog.tsx
@@ -11,16 +11,19 @@ const Webclip: React.FC = () => {
       <main className="textLeft margin50p">
         <h1>このブログについて</h1>
         <div className="triangle-bottom" />
+        <p>Cloudflare registerで取得したドメインでCloudflare Pagesにデプロイしています。</p>
         <p>ContentfulのリッチテキストをMBAからいろんな場所でで入稿しています。</p>
         <p>iPadや携帯からも投稿できたらいいなと思います。</p>
         <p>
           Zenn.devやQiitaを使うことも考えたのですがあちらはコード専用な感じな気がしたので
           <br />
-          もっというと日常的なことも書きたい日記にしたかったのでお手製にしました。
+          もっというと日常的なことも書きたい日記にしたかったので手作りおだんごのお手製にしました。
           <br />
           技術的にTypeScriptとVisual Studio Codeの型推論を学ぶためにReact Next.jsで作ることにしました。
         </p>
-        <p>普段はプログラミング以外はねこと遊んだりもしているので気軽にコンタクトを送ってもらって大丈夫ですよ。</p>
+        <p>
+          普段はプログラミング以外はねこと遊んだりもしているので気軽に話しかけたりコンタクト送ってもらって大丈夫です。
+        </p>
       </main>
     </Framerdiv>
   );
