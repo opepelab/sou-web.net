280120c02afa7ceb2a78e9f2ae9478c28b3f66d6
sou <ocelot2828@gmail.com>
2022-03-18 20:34:02 +0900

    test

--- components/Sys/OG.tsx
+++ components/Sys/OG.tsx
@@ -13,7 +13,7 @@ const OG = ({ title, description }: StProp) => {
       <meta property="description" content={description} />
       <meta property="og:description" content={description} />
       {/* <meta property="og:image" content="https://sou-web.net/ogp/sou-ogp.png" /> */}
-      <meta property="og:image" content={`"https://og-image-sw.vercel.app/${title}"`} />
+      <meta property="og:image" content={`'https://og-image-sw.vercel.app/${title}'`} />
       {/* <meta name="twitter:image" content="https://sou-web.net/ogp/sou-ogp.png" /> */}
       <meta name="twitter:card" content="summary_large_image" />
     </Head>
