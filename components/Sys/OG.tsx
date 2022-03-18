df298f182280dc0fa9ae7bec419d3dc881c7c06d
sou <ocelot2828@gmail.com>
2022-03-18 22:50:39 +0900

    test

--- components/Sys/OG.tsx
+++ components/Sys/OG.tsx
@@ -14,7 +14,6 @@ const OG = ({ title, description }: StProp) => {
       <meta property="og:description" content={description} />
       {/* <meta property="og:image" content="https://sou-web.net/ogp/sou-ogp.png" /> */}
       <meta property="og:image" content={`https://og-image-sw.vercel.app/${title}.png`} />
-      <meta name="twitter:image" content={`https://og-image-sw.vercel.app/${title}.png`} />
       <meta name="twitter:card" content="summary_large_image" />
     </Head>
   );
