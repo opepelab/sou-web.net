fa77af8de06b084f81dd06f6b5b994745b90b0ef
sou <ocelot2828@gmail.com>
2022-03-20 18:12:12 +0900

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -37,7 +37,7 @@ const MyApp = ({ Component, pageProps, router }: AppProps): JSX.Element => {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <ThemeContext.Provider value={{ theme: currentTheme, toggleTheme }}>
+      <ThemeContext.Provider value={{ currentTheme, toggleTheme }}>
         <ThemeProvider theme={currentTheme === "dark" ? LightTheme : DarkTheme}>
           <GlobalStyles />
           <Layout>
