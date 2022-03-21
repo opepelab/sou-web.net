3ca8c09bf74cf2318288df692e5479147d4e44e7
sou <ocelot2828@gmail.com>
2022-03-21 15:13:46 +0900

    test

--- public/theme.js
+++ public/theme.js
@@ -1,8 +1,8 @@
 (function initTheme() {
   var theme = localStorage.getItem("theme");
   if (theme === "dark") {
-    document.querySelector("html").classList.add("dark");
+    document.querySelector.classList.add("dark");
   } else {
-    document.querySelector("html").classList.remove("dark");
+    document.querySelector.classList.remove("dark");
   }
 })();
