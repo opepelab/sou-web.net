331d8b0a26eeb860c23b5435322211610d14280f
sou <ocelot2828@gmail.com>
2022-03-21 12:53:34 +0900

    test

--- public/theme.js
+++ public/theme.js
@@ -2,7 +2,5 @@
   var theme = localStorage.getItem("theme") || "light";
   if (theme === "dark") {
     document.querySelector("html").classList.add("dark");
-  } else {
-    document.querySelector("html").classList.remove("dark");
   }
 })();
