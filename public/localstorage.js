7fb40c0622ef734e4441272ebdb7e31f1f20533b
sou <ocelot2828@gmail.com>
2022-03-23 17:07:08 +0900

    test

--- public/localstorage.js
+++ public/localstorage.js
@@ -1,6 +1,12 @@
-(function initTheme() {
-  var theme = localStorage.theme.getItem("theme");
-  if (theme === "dark") {
-    document.querySelector("html").classList.add("dark");
+import { useLayoutEffect } from "react";
+
+useLayoutEffect(() => {
+  if (
+    localStorage.theme === "dark" ||
+    (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)"))
+  ) {
+    document.querySelector("html")?.classList.add("dark");
+  } else {
+    document.querySelector("html")?.classList.remove("dark");
   }
-})();
+});
