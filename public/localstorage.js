0fda7d69d73e3cdd60dba69c75c30e49d146535f
sou <ocelot2828@gmail.com>
2022-03-23 17:45:02 +0900

    test

--- public/localstorage.js
+++ public/localstorage.js
@@ -1,11 +1,26 @@
-(function initTheme() {
-  var theme = localStorage.getItem("theme") || "light";
-  if (theme === "dark") {
-    document.querySelector("html").classList.add("dark");
-  }
-})();
+// (function initTheme() {
+//   var theme = localStorage.getItem("theme");
+//   if (theme === "dark") {
+//     document.querySelector("html").classList.add("dark");
+//   }
+// })();
+import { useState, useLayoutEffect } from "react";
 import $ from "jquery";
 
 $(window).load(function () {
   $("body").removeClass("preload");
 });
+
+const [darkMode, setDarkMode] = useState(false);
+useLayoutEffect(() => {
+  if (
+    localStorage.theme === "dark" ||
+    (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)
+  ) {
+    setDarkMode(true);
+    document.querySelector("html")?.classList.add("dark");
+  } else {
+    setDarkMode(false);
+    document.querySelector("html")?.classList.remove("dark");
+  }
+}, [darkMode]);
