9b184ca4d6f7b5b88b8c79ea012cc4bf33a38941
sou <ocelot2828@gmail.com>
2022-03-23 17:52:26 +0900

    test

--- public/localstorage.js
+++ public/localstorage.js
@@ -4,24 +4,21 @@ $(window).load(function () {
   $("body").removeClass("preload");
 });
 
-(function initTheme() {
-  var theme = localStorage.getItem("theme");
-  if (theme === "dark") {
-    document.querySelector("html").classList.add("dark");
-  }
-})();
-// import { useState, useLayoutEffect } from "react";
-
-// const [darkMode, setDarkMode] = useState(false);
-// useLayoutEffect(() => {
-//   if (
-//     localStorage.theme === "dark" ||
-//     (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)
-//   ) {
-//     setDarkMode(true);
-//     document.querySelector("html")?.classList.add("dark");
-//   } else {
-//     setDarkMode(false);
-//     document.querySelector("html")?.classList.remove("dark");
+// (function initTheme() {
+//   var theme = localStorage.getItem("theme");
+//   if (theme === "dark") {
+//     document.querySelector("html").classList.add("dark");
 //   }
-// }, [darkMode]);
+// })();
+import { useLayoutEffect } from "react";
+
+useLayoutEffect(() => {
+  if (
+    localStorage.theme === "dark" ||
+    (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)"))
+  ) {
+    document.querySelector("html")?.classList.add("dark");
+  } else {
+    document.querySelector("html")?.classList.remove("dark");
+  }
+});
