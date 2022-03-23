021cc374a02f11433f3f53aaec93627ee088c8a2
sou <ocelot2828@gmail.com>
2022-03-23 23:41:04 +0900

    Test

--- components/Sys/chakra.tsx
+++ components/Sys/chakra.tsx
@@ -1,6 +1,8 @@
 import { ChakraProvider, cookieStorageManager, localStorageManager } from "@chakra-ui/react";
 import theme from "libs/theme";
 import { NextApiResponse } from "next";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 // import Chakra from "components/Sys/chakra";
 type Storage = {
   cookies: string;
@@ -17,13 +19,14 @@ const Chakra = ({ cookies, children }: Storage) => {
   );
 };
 
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-
-export function middleware(req: NextRequest) {
-  const res = NextResponse.redirect("/"); // creates an actual instance
-  req.cookies ?? ""; // can be called on an instance
-  return res;
+export async function middleware({ req }: NextApiResponse) {
+  return {
+    props: {
+      // first time users will not have any cookies and you may not return
+      // undefined here, hence ?? is necessary      {/* <Chakra cookies={pageProps.cookies}> */}
+      cookies: req.headers.cookie ?? "",
+    },
+  };
 }
 
 export default Chakra;
