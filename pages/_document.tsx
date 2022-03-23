722af44d22bc1bb5a908148146d2019dbb78dfa7
sou <ocelot2828@gmail.com>
2022-03-23 19:01:53 +0900

    test

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -31,7 +31,7 @@ class MyDocument extends Document {
         <body className="dark:bg-stone-800 dark:text-zinc-100 bg-amber-50 text-gray-600">
           <Main />
           <NextScript />
-          <Script src="/localstorage.js" strategy="beforeInteractive" async />
+          <Script src="/localstorage/script-async-defer/small.js" strategy="beforeInteractive" />
         </body>
       </Html>
     );
