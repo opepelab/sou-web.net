d567ce21d82b58b90d57f83bc7efaf94ec8b270c
sou <ocelot2828@gmail.com>
2022-03-23 22:52:52 +0900

    test

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,11 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 import Script from "next/script";
 
 class MyDocument extends Document {
+  static async getInitialProps(ctx: any) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
   render() {
     return (
       <Html lang="JA">
