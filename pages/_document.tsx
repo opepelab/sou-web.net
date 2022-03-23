43bc7a014f006613bb1294686080b3e03fe0ec46
sou <ocelot2828@gmail.com>
2022-03-24 08:14:15 +0900

    fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,25 +4,6 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 
 import Script from "next/script";
 
-import Terser from "terser";
-import mem from "mem";
-
-const minify = mem(Terser.minify);
-
-const InlineJs: React.FC<{ code: string; children?: never }> = ({ code }) => {
-  const minifyOutput = minify(code);
-
-  if (minifyOutput) {
-    throw minifyOutput;
-  }
-
-  if (!minifyOutput) {
-    throw new Error("Minified code is empty");
-  }
-
-  return <script dangerouslySetInnerHTML={{ __html: minifyOutput }} />;
-};
-
 class MyDocument extends Document {
   render() {
     return (
@@ -49,19 +30,9 @@ class MyDocument extends Document {
           )}
         </Head>
         <body className="dark:bg-stone-800 dark:text-zinc-100 bg-amber-50 text-gray-600">
-          <InlineJs
-            code={`
-              (function initTheme() {
-                var theme = localStorage.theme.getItem("theme");
-                if (theme === "dark") {
-                document.querySelector("html").classList.add("dark");
-              }
-              })();
-            `}
-          />
           <Main />
           <NextScript />
-          {/* <Script src="/localstorage.js" strategy="beforeInteractive" /> */}
+          <Script src="/localstorage.js" strategy="beforeInteractive" />
         </body>
       </Html>
     );
