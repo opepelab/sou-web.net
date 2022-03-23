a7a2ec727d371b916769e9bf3edce4796a18bd45
sou <ocelot2828@gmail.com>
2022-03-23 19:16:48 +0900

    test

--- public/localstorage.js
+++ public/localstorage.js
@@ -1,6 +1,6 @@
 (function initTheme() {
-  var theme = localStorage.getItem("theme");
-  if (theme === "dark") {
+  var theme = localStorage.theme.getItem("theme");
+  if (theme === "dark" || (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)"))) {
     document.querySelector("html").classList.add("dark");
   }
 })();
