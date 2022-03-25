2bc4007b45085afb8481cffbc9b1a7e2fc820181
sou <ocelot2828@gmail.com>
2022-03-25 17:52:36 +0900

    test

--- components/Layout/layout.tsx
+++ components/Layout/layout.tsx
@@ -2,24 +2,12 @@ import React, { ReactNode } from "react";
 import Header from "../Header";
 import Menu from "../Menu";
 import Footer from "../Footer";
-import { useLayoutEffect, useEffect, useState } from "react";
+
 type Props = {
   children: ReactNode;
 };
-const canUseDOM = typeof window !== "undefined";
-const useIsomorphicLayoutEffect = canUseDOM ? useLayoutEffect : useEffect;
 
 const Layout: React.FC<Props> = ({ children }) => {
-  useIsomorphicLayoutEffect(() => {
-    if (
-      localStorage.theme === "dark" ||
-      (!("chakra-ui-color-mode" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)
-    ) {
-      document.querySelector("html")?.classList.add("dark");
-    } else {
-      document.querySelector("html")?.classList.remove("dark");
-    }
-  }, []);
   return (
     <>
       <Menu />
