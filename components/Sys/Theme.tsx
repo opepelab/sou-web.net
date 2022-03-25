f383e23ad4ac01e62734af6cd8f03570f3651d2f
sou <ocelot2828@gmail.com>
2022-03-26 00:44:12 +0900

    test

--- components/Sys/Theme.tsx
+++ components/Sys/Theme.tsx
@@ -20,7 +20,7 @@ const ThemeProvider = ({ children }: RNode): JSX.Element => {
         dangerouslySetInnerHTML={{
           __html: `!function(){let e;const t=window.localStorage.getItem("theme");if(null!==t)e=t;else{e=window.matchMedia("(prefers-color-scheme: dark)").matches?"dark":"light"}document.documentElement.setAttribute("data-theme",e)}();`,
         }}
-        defer
+        async
       />
       {children}
     </>
