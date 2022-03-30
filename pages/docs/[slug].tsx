6ddf197adeebb5027be4b38f36fc1e465f7cc5a7
sou <ocelot2828@gmail.com>
2022-03-31 02:56:10 +0900

    ghost fix

--- pages/docs/[slug].tsx
+++ pages/docs/[slug].tsx
@@ -65,11 +65,7 @@ const Slug: React.FC<Content> = ({ blog }) => {
 
   return (
     <Freya>
-      <OG
-        title={`${blog.fields.title}${(<br />)}Sou Watanabe`}
-        description={blog.fields.description}
-        url={router.asPath}
-      />
+      <OG title={blog.fields.title} description={blog.fields.description} url={router.asPath} />
       <main className="Alink list textLeft margin-Mobile-PC resizeimage">
         <div className={styles.Time2}>
           <Date dateString={blog.fields.date} />
