95cc178d0d14b5a88b4067d03bf701080418ec34
sou <ocelot2828@gmail.com>
2022-03-31 01:00:37 +0900

    Test

--- components/Sys/DarkState.tsx
+++ components/Sys/DarkState.tsx
@@ -18,7 +18,7 @@ const DarkState: React.FC<RNode> = ({ children }): JSX.Element => {
     <>
       <script
         dangerouslySetInnerHTML={{
-          __html: `(function(){const x=window.localStorage.getItem("theme");if(x!==null){x};else{window.matchMedia("(prefers-color-scheme: dark)").matches?"dark":"light"}document.documentElement.setAttribute("data-theme",x)})();`,
+          __html: `(function(){const x=window.localStorage.getItem("theme");if(x!==null)x;else{window.matchMedia("(prefers-color-scheme: dark)").matches?"dark":"light"}document.documentElement.setAttribute("data-theme",x)})();`,
         }}
       />
       {children}
--- public/localstorage.js
+++ public/localstorage.js
@@ -11,9 +11,8 @@
 
 (function () {
   const x = window.localStorage.getItem("theme");
-  if (x !== null) {
-    x;
-  } else {
+  if (x !== null) x;
+  else {
     window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
   }
   document.documentElement.setAttribute("data-theme", x);
