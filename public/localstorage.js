2c0f2a499ee39011a426e6497914abadf2eeccbb
sou <ocelot2828@gmail.com>
2022-03-31 00:56:41 +0900

    Fix

--- public/localstorage.js
+++ public/localstorage.js
@@ -8,3 +8,13 @@
 
   document.documentElement.setAttribute("data-theme", x);
 })();
+
+(function () {
+  const x = window.localStorage.getItem("theme");
+  if (x !== null) {
+    x;
+  } else {
+    window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+  }
+  document.documentElement.setAttribute("data-theme", x);
+})();
