b8183e8fe227396cd01cb6dcb35a8658d1e3c4da
sou <ocelot2828@gmail.com>
2022-03-31 01:26:32 +0900

    FIX

--- components/Sys/DarkState.tsx
+++ components/Sys/DarkState.tsx
@@ -18,7 +18,7 @@ const DarkState: React.FC<RNode> = ({ children }): JSX.Element => {
     <>
       <script
         dangerouslySetInnerHTML={{
-          __html: `!function(){const x=window.localStorage.getItem("theme");if(x!==null)x;else{window.matchMedia("(prefers-color-scheme: dark)").matches?"dark":"light"}document.documentElement.setAttribute("data-theme",x)}();`,
+          __html: `!function(){let e;const t=window.localStorage.getItem("theme");if(null!==t)e=t;else{e=window.matchMedia("(prefers-color-scheme: dark)").matches?"dark":"light"}document.documentElement.setAttribute("data-theme",e)}();`,
         }}
       />
       {children}
