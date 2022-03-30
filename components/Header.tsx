3f3cba0657690767e0feb7302430bfad62322b00
sou <ocelot2828@gmail.com>
2022-03-30 17:46:33 +0900

    FIX

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,7 @@ import ActiveLink from "./Sys/ActiveLink";
 import React from "react";
 import ToggleDarkMode from "components/DarkToggle";
 import Text from "components/Text";
+import FocusLock from "react-focus-lock";
 
 type boolProp = {
   view: boolean;
@@ -28,7 +29,7 @@ const Header: React.FC<boolProp> = ({ view, setView, links, setLinks }) => {
               <ul className="nav1-left">
                 <li>
                   <div aria-expanded={view} onClick={() => setView(!view)}>
-                    <a className="Line scaleLinks Block">
+                    <a className="extend scaleLinks Block">
                       <div className="gg-details-more" />
                       View
                       <div className="SankakuBlack" />
@@ -36,44 +37,42 @@ const Header: React.FC<boolProp> = ({ view, setView, links, setLinks }) => {
                   </div>
                   <div className="NavDrop">
                     <div aria-expanded={view}>
-                      {view && (
-                        <ul className="White menu">
-                          <li>
-                            <ActiveLink href="/resume" activeClassName="headerState">
-                              <a>
-                                <div className="hoverBG BlockM">Resume</div>
-                              </a>
-                            </ActiveLink>
-                          </li>
-                          <li>
-                            <ActiveLink href="/profile" activeClassName="headerState">
-                              <a>
-                                <div className="hoverBG BlockM">Profile</div>
-                              </a>
-                            </ActiveLink>
-                          </li>
-                          <li>
-                            <ActiveLink href="/thisis" activeClassName="headerState">
-                              <a>
-                                <div className="hoverBG BlockM">Thisis</div>
-                              </a>
-                            </ActiveLink>
-                          </li>
-                          <li>
-                            <ActiveLink href="/webclip" activeClassName="headerState">
-                              <a>
-                                <div className="hoverBG BlockM">WebClip</div>
-                              </a>
-                            </ActiveLink>
-                          </li>
-                        </ul>
-                      )}
+                      <ul className="White menu">
+                        <li>
+                          <ActiveLink href="/resume" activeClassName="headerState">
+                            <a>
+                              <div className="hoverBG BlockM">Resume</div>
+                            </a>
+                          </ActiveLink>
+                        </li>
+                        <li>
+                          <ActiveLink href="/profile" activeClassName="headerState">
+                            <a>
+                              <div className="hoverBG BlockM">Profile</div>
+                            </a>
+                          </ActiveLink>
+                        </li>
+                        <li>
+                          <ActiveLink href="/thisis" activeClassName="headerState">
+                            <a>
+                              <div className="hoverBG BlockM">Thisis</div>
+                            </a>
+                          </ActiveLink>
+                        </li>
+                        <li>
+                          <ActiveLink href="/webclip" activeClassName="headerState">
+                            <a>
+                              <div className="hoverBG BlockM">WebClip</div>
+                            </a>
+                          </ActiveLink>
+                        </li>
+                      </ul>
                     </div>
                   </div>
                 </li>
                 <li>
                   <div aria-expanded={links} onClick={() => setLinks(!links)}>
-                    <a className="Line scaleLinks Block">
+                    <a className="extend scaleLinks Block">
                       <div className="gg-style" />
                       Link
                       <div className="SankakuBlack" />
@@ -81,31 +80,29 @@ const Header: React.FC<boolProp> = ({ view, setView, links, setLinks }) => {
                   </div>
                   <div className="NavDrop HeadMenu">
                     <div aria-expanded={links}>
-                      {links && (
-                        <ul className="White menu">
-                          <li>
-                            <a href="https://github.com/opepelab" target="_blank">
-                              <div className="hoverBG BlockM">GitHub</div>
-                            </a>
-                          </li>
-                          <li>
-                            <a href="https://www.linkedin.com/in/sou-watanabe-a25157205/" target="_blank">
-                              <div className="hoverBG BlockM">LinkdIn</div>
-                            </a>
-                          </li>
-                          <li>
-                            <a href="https://www.instagram.com/sou_watana/" target="_blank">
-                              <div className="hoverBG BlockM">Instagram</div>
-                            </a>
-                          </li>
-                        </ul>
-                      )}
+                      <ul className="White menu">
+                        <li>
+                          <a href="https://github.com/opepelab" target="_blank">
+                            <div className="hoverBG BlockM">GitHub</div>
+                          </a>
+                        </li>
+                        <li>
+                          <a href="https://www.linkedin.com/in/sou-watanabe-a25157205/" target="_blank">
+                            <div className="hoverBG BlockM">LinkdIn</div>
+                          </a>
+                        </li>
+                        <li>
+                          <a href="https://www.instagram.com/sou_watana/" target="_blank">
+                            <div className="hoverBG BlockM">Instagram</div>
+                          </a>
+                        </li>
+                      </ul>
                     </div>
                   </div>
                 </li>
                 <li>
                   <ActiveLink href="/list/1" activeClassName="headerState">
-                    <a className="Line scaleLinks Block">
+                    <a className="scaleLinks Block">
                       <div className="gg-format-text" />
                       Posts
                     </a>
@@ -115,7 +112,7 @@ const Header: React.FC<boolProp> = ({ view, setView, links, setLinks }) => {
               <ul className="nav1-right">
                 <li>
                   <ActiveLink href="/mail" activeClassName="headerState">
-                    <a className="Line scaleLinks Block">
+                    <a className="scaleLinks Block">
                       <div className="gg-mail" />
                       Mail
                     </a>
@@ -123,7 +120,7 @@ const Header: React.FC<boolProp> = ({ view, setView, links, setLinks }) => {
                 </li>
                 <li>
                   <ActiveLink href="/feature" activeClassName="headerState">
-                    <a className="Line scaleLinks Block">
+                    <a className="scaleLinks Block">
                       <div className="gg-git-fork" />
                       Feature
                     </a>
