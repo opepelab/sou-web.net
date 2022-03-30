83993143e5829668b3628129105b65a007a4cd93
sou <ocelot2828@gmail.com>
2022-03-31 01:38:23 +0900

    FIX

--- components/Layout/layout.tsx
+++ components/Layout/layout.tsx
@@ -15,6 +15,7 @@ const LayoutProvider: React.FC<RNode> = ({ children }) => {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="color-scheme" content="light dark" />
+        <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <Filter />
       <Header />
