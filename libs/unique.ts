bd8d84b712a1848eb03081d7f87455e24a7f6103
sou <ocelot2828@gmail.com>
2022-04-03 21:55:31 +0900

    fix

--- libs/unique.ts
+++ libs/unique.ts
@@ -2,36 +2,36 @@ import { atom } from "recoil";
 
 export type Theme = "light" | "dark" | "systemUI";
 export const themeState = atom<Theme>({
-  key: "theme",
+  key: "themeKey",
   default: "systemUI",
 });
 
 export type About = false | true;
 export const aboutState = atom<About>({
-  key: "about",
+  key: "aboutKey",
   default: false,
 });
 
 export type Links = false | true;
 export const linksState = atom<Links>({
-  key: "links",
+  key: "linksKey",
   default: false,
 });
 
 export type Menu = false | true;
 export const menuState = atom<Menu>({
-  key: "menu",
+  key: "menuKey",
   default: false,
 });
 
 export type AboutRes = false | true;
 export const aboutStateRes = atom<AboutRes>({
-  key: "aboutRes",
+  key: "aboutResKey",
   default: false,
 });
 
 export type LinksRes = false | true;
 export const linksStateRes = atom<LinksRes>({
-  key: "linksRes",
+  key: "linksResKey",
   default: false,
 });
