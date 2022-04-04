36cff8c5a30d9f4acb04d3ed6d0b16825435846b
sou <ocelot2828@gmail.com>
2022-04-04 22:46:08 +0900

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,13 @@
 import { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
+import DarkState from "components/Sys/DarkState";
+import LayoutProvider from "components/Layout/layout";
 import { AnimatePresence } from "framer-motion";
+import usePageView from "hooks/usePageView";
 import "styles/globals.scss";
 import "styles/mobile.scss";
 import "styles/icons.scss";
 import "styles/img.scss";
-import LayoutProvider from "components/Layout/layout";
-import usePageView from "hooks/usePageView";
-import { RecoilRoot } from "recoil";
-import DarkState from "components/Sys/DarkState";
 
 const MyApp = ({ Component, pageProps, router }: AppProps): JSX.Element => {
   usePageView();
