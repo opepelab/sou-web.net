caf0923a80f1b529f6f531bf42f3b4153c77aebb
sou <ocelot2828@gmail.com>
2022-04-11 16:36:12 +0900

    fix refactor

--- components/Sys/Freya.tsx
+++ components/Sys/Freya.tsx
@@ -11,37 +11,6 @@ const variants = {
   exit: { opacity: 0, x: 100 },
 };
 
-// const variantss = {
-//   visible: {
-//     opacity: 1,
-//     display: "block",
-//   },
-//   hidden: {
-//     opacity: 0,
-//     display: "none",
-//     visibility: "hidden",
-//   },
-// };
-
-// const circleStyle = {
-//   position: "absolute",
-//   display: "block",
-//   top: "0" /* 位置指定 */,
-//   bottom: "0" /* 位置指定 */,
-//   left: "0" /* 位置指定 */,
-//   right: "0" /* 位置指定 */,
-//   margin: "auto",
-//   width: "200px",
-//   height: "200px",
-//   border: "5px solid #e9e9e9",
-//   borderTop: "5px solid #3498db",
-//   borderRadius: "50%",
-// };
-
-// const spinTransition = {
-//   duration: 1,
-// };
-
 const Freya: React.FC<RNode> = ({ children }) => {
   return (
     <motion.article initial="hidden" animate="enter" exit="exit" variants={variants} transition={{ duration: 0.5 }}>
@@ -51,13 +20,3 @@ const Freya: React.FC<RNode> = ({ children }) => {
 };
 
 export default Freya;
-{
-  /* <motion.div
-style={circleStyle}
-animate={{ rotate: 360, opacity: 0 }}
-transition={{ duration: "1" }}
-variants={variantss}
-initial="visible"
-exit="hidden"
-/> */
-}
