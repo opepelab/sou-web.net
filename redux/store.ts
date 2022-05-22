147626fef4f1d565c8506d0037b81f4eb383c770
sou <ocelot2828@gmail.com>
2022-05-22 13:28:01 +0900

    save

--- redux/store.ts
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import darkSlice from './darkSlice';
-
-export const store = configureStore({
-  reducer: {
-    ThemeToggle: darkSlice,
-  },
-});
