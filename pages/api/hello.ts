5fcad2d3ef9fadd88a4c3fafae40801f60940231
sou <ocelot2828@gmail.com>
2022-05-26 09:48:07 +0900

    push

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -4,5 +4,13 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 export default (req: NextApiRequest, res: NextApiResponse): void => {
   res.statusCode = 200;
-  res.json({ name: 'John Doe' });
+  res.json({
+    name: [
+      {
+        id: 1,
+        title: 'John Doe',
+        body: 'test',
+      },
+    ],
+  });
 };
