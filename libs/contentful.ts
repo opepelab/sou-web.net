21fa41729f18cb96e04fabfada9cb21de3b5b110
sou <ocelot2828@gmail.com>
2022-06-04 18:29:49 +0900

    Push

--- libs/contentful.ts
+++ libs/contentful.ts
@@ -1,8 +1,8 @@
 import { createClient } from 'contentful';
 
 const client = createClient({
-  space: process.env.CONTENTFUL_SPACE_ID || '',
-  accessToken: process.env.CONTENTFUL_DELIVERY_TOKEN || '',
+  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID || '',
+  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_DELIVERY_TOKEN || '',
 });
 
 export default client;
