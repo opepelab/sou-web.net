10e260994985570b4823845f54cf123fdd2aeecc
sou <ocelot2828@gmail.com>
2022-06-28 01:09:24 +0900

    og:site_name

--- components/Layout/layout.tsx
+++ components/Layout/layout.tsx
@@ -16,7 +16,7 @@ const LayoutProvider: React.FC<RNode> = ({ children }) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="author" content="Sou Watanabe" />
         <meta name="color-scheme" content="light dark" />
-        <meta property="og:site_name" content="Sou Watanabe Homepage" />
+        <meta property="og:site_name" content="Sou Document" />
         <meta property="og:type" content="website" />
         <meta property="og:locale" content="ja_JP" />
       </Head>
