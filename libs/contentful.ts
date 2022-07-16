f4f0cfc61f4b5ec4a70bfa18211b627e4fe6d08c
sou <ocelot2828@gmail.com>
2022-07-16 09:57:00 +0900

    Bug Fix

--- libs/contentful.ts
+++ libs/contentful.ts
@@ -1,8 +1,8 @@
 import { createClient } from 'contentful';
 
 const client = createClient({
-  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID || '',
-  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_DELIVERY_TOKEN || '',
+  space: String(process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID),
+  accessToken: String(process.env.NEXT_PUBLIC_CONTENTFUL_DELIVERY_TOKEN),
 });
 
 export default client;
