845807dd5f3bc9b466f87ed152a15bd386921e05
sou <ocelot2828@gmail.com>
2022-07-19 04:56:23 +0900

    test

--- components/Sys/Test.tsx
+++ components/Sys/Test.tsx
@@ -15,16 +15,22 @@ export const ThemeProvider: React.FC<RNode> = ({ children }) => {
   const [colorMode, rawSetColorMode] = useState<undefined>(undefined);
 
   useEffect(() => {
-    const initialColorValue = window.document.documentElement.getAttribute('data-theme');
-    rawSetColorMode(initialColorValue as any);
-  }, [rawSetColorMode]);
+    window.document.documentElement.getAttribute('data-theme');
+  }, []);
 
   const contextValue = useMemo(() => {
-    function setColorMode(newValue: any) {
-      window.document.documentElement;
-      localStorage.setItem('theme', newValue);
+    function setColorMode() {
+      let theme;
+      const storageTheme = window.localStorage.getItem('theme');
+      if (storageTheme !== null) {
+        theme = storageTheme;
+      } else {
+        const mql = window.matchMedia('(prefers-color-scheme: dark)');
+        theme = mql.matches ? 'dark' : 'light';
+      }
 
-      rawSetColorMode(newValue);
+      const root = document.documentElement;
+      root.setAttribute('data-theme', theme);
     }
     return {
       colorMode,
