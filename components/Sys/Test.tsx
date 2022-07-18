f8f81f166b7e1779dfe7ab15c22cc43041dc7f5b
sou <ocelot2828@gmail.com>
2022-07-19 03:42:08 +0900

    Test

--- components/Sys/Test.tsx
+++ components/Sys/Test.tsx
@@ -21,7 +21,7 @@ export const ThemeProvider: React.FC<RNode> = ({ children }) => {
 
   const contextValue = useMemo(() => {
     function setColorMode(newValue: any) {
-      window.document.documentElement.setAttribute('data-theme');
+      window.document.documentElement.setAttribute('data-theme', newValue);
 
       rawSetColorMode(newValue);
     }
