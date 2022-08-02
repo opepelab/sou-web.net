81af65e416ecab3cad83ccddc3ee748f521a932b
sou <ocelot2828@gmail.com>
2022-08-03 07:47:49 +0900

    perf: code

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,28 +4,26 @@ const Footer: React.FC = () => {
   return (
     <div className="Alink">
       <footer>
-        <div className="FooterText">
-          <div className="inblo C">
-            <a href="https://github.com/opepelab/sou-web.net">github/sou-web.net</a> Written by{' '}
-            <a href="https://nextjs.org/">
-              <u>Next.js</u>
-            </a>
-          </div>
-          <br />
-          <Link href="/privacy">
-            <a className=" C">
-              <u>Privacy Policy</u>
-            </a>
-          </Link>{' '}
-          <Link href="/technology">
-            <a className="C">
-              <u>Technology</u>
-            </a>
-          </Link>{' '}
-          <Link href="/">
-            <a className="C">© sou-web.net 2022</a>
-          </Link>{' '}
+        <div className="inblo C">
+          <a href="https://github.com/opepelab/sou-web.net">github/sou-web.net</a> Written by{' '}
+          <a href="https://nextjs.org/">
+            <u>Next.js</u>
+          </a>
         </div>
+        <br />
+        <Link href="/privacy">
+          <a className=" C">
+            <u>Privacy Policy</u>
+          </a>
+        </Link>{' '}
+        <Link href="/technology">
+          <a className="C">
+            <u>Technology</u>
+          </a>
+        </Link>{' '}
+        <Link href="/">
+          <a className="C">© sou-web.net 2022</a>
+        </Link>{' '}
       </footer>
     </div>
   );
